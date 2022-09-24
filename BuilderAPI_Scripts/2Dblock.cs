cb75605787deb8f7d2cc835adcd2bcf0bf1357fe
GKGamerRU <superkan224@gmail.com>
2022-09-24 10:56:06 +0300

    Builder API 2.0

---
+++ BuilderAPI_Scripts/2Dblock.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LB_Launcher.Builder2D;
+using System.Windows.Forms;
+using System.Drawing;
+using CharpShell;
+using LB_Launcher.Modules;
+
+namespace LB_Launcher
+{
+    public static class _2Dblock
+    {
+        public const string VERSION = "2.0";
+        public static List<Block> GetBlocks() { return Engine.Cubes; }
+        public static List<Block> GetBlockByIDTexture(int id)
+        {
+            List<Block> _blocks = new List<Block>();
+            foreach (Block cube in Engine.Cubes)
+            {
+                if (cube._IndexTexture == id) _blocks.Add(cube);
+            }
+            return _blocks;
+        }
+        public static Block GetBlockByID(int id)
+        { return Engine.Cubes[id]; }
+        public static Block GetBlockByName(string name)
+        { foreach (Block cube in Engine.Cubes) { if (cube.BlockMod.BlockName == name) return cube; }; return null; }
+        public static IEnumerable<Block> GetBlockByNameArray(string name)
+        { return Engine.Cubes.Where(block => block.BlockMod.BlockName == name); }
+        public static IEnumerable<Block> GetBlockByModNameArray(string Modname)
+        { return Engine.Cubes.Where(block => block.Mod.Name == Modname); }
+        public static Panel PaintTable() { return (Panel)thisForm.Controls.Find("panel2", true).FirstOrDefault(); }
+        public static Panel LoadPanel() { return (Panel)thisForm.Controls.Find("panel6", true).FirstOrDefault(); }
+        public static Form Form() { return thisForm; }
+        public static string GetCurrentModInfo() { return SelectedInv.BlockName + "\n" + SelectedInv.ModName; }
+        public static Graphics GetGFX() { return Engine.gfx; }
+        public static Block GetThisBlock() { return Engine.Cubes.LastOrDefault(); }
+        public static Image GetBackground() { return Engine.CurrentGround; }
+        public static Inventar GetSelectedInventar() { return Builder2D.SelectedInv; }
+        public static Control GetCounter() { return thisForm.Controls.Find("counterblock", true).FirstOrDefault(); }
+        public static Block TryBuild(int x, int y, ModG pattern)
+        {
+            List<Block> _list = _2Dblock.GetBlocks();
+            bool canbuild = true;
+
+            foreach (Block WTfT in _list)
+            {
+                if (WTfT.Position.x == x-1 && WTfT.Position.y == y+1) { canbuild = false; break; }
+            }
+
+            if (canbuild)
+            {
+                return Build(x, y, pattern);
+            }
+            return null;
+        }
+        public static void Destroy(Block obj)
+        {
+            var CurrentGround = _2Dblock.GetBackground();
+            var Cubes = _2Dblock.GetBlocks();
+            var gfx = _2Dblock.GetGFX();
+            Point timedBlock = new Point(obj.Position.x,obj.Position.y);
+
+            if (CurrentGround != null)
+            {
+                obj.Destroy();
+                Cubes.Remove(obj);
+                gfx.FillRectangle(SystemBrushes.ActiveCaption, timedBlock.X, timedBlock.Y, obj.Scale.x, obj.Scale.y);
+                gfx.DrawImage(CurrentGround, timedBlock);
+            }
+            else
+            {
+                obj.Destroy();
+                Cubes.Remove(obj);
+                gfx.FillRectangle(SystemBrushes.ActiveCaption, timedBlock.X, timedBlock.Y, obj.Scale.x, obj.Scale.y);
+            }
+
+            Render();
+        }
+        public static void Destroy(int x, int y)
+        {
+            var CurrentGround = _2Dblock.GetBackground();
+            var Cubes = _2Dblock.GetBlocks();
+            var gfx = _2Dblock.GetGFX();
+            Point timedBlock = new Point(x-1, y+1);
+
+            foreach(var block in Cubes)
+            {
+                if (block == null) continue;
+                if (timedBlock == new Point(block.Position.x, block.Position.y))
+                {
+                    if (CurrentGround != null)
+                    {
+                        block.Destroy();
+                        Cubes.Remove(block);
+                        gfx.FillRectangle(SystemBrushes.ActiveCaption, timedBlock.X, timedBlock.Y, block.Scale.x, block.Scale.y);
+                        gfx.DrawImage(CurrentGround, timedBlock);
+                        break;
+                    }
+                    else
+                    {
+                        block.Destroy();
+                        Cubes.Remove(block);
+                        gfx.FillRectangle(SystemBrushes.ActiveCaption, timedBlock.X, timedBlock.Y, block.Scale.x, block.Scale.y);
+                        break;
+                    }
+                }
+            }
+        }
+        public static void DestroyAll()
+        {
+            Builder2D.thisForm.Erase();
+        }
+        public static Block Build(int x, int y, ModG pattern)
+        {
+            List<Block> _list = _2Dblock.GetBlocks();
+            Graphics _er = _2Dblock.GetGFX();
+            Image _s = pattern._Texture;
+            
+            Block newel = new Block(ref _er, _s, new Point(x-1, y+1), 0, pattern.Mod, pattern, pattern.OnRedraw, pattern.OnDestroyed, pattern.OnClick);
+            _list.Add(newel);
+            newel.draw();
+            return newel;
+        }
+        public static void Render() { Builder2D.Engine.ApplyRender(); }
+    }
+    public class Block
+    {
+        public Action<Block> OnRedraw = delegate(Block obj) { };
+        public Action<Block> OnDestroy = delegate(Block obj) { };
+        public Action<Block, int> OnClick = delegate (Block obj, int click) { };
+        public Vector2 Position;
+        public Vector2 Scale;
+        public Image Texture;
+        Graphics _GFX; 
+        public int _IndexTexture;
+        public ModManager.Mod Mod;
+        public ModG BlockMod;
+        public bool IsInteractive = false;
+        public Block(ref Graphics _Parent, Image _texture, Point _pos, int index, ModManager.Mod _mod, ModG blockName, Action<Block> redraw = null, Action<Block> destroy = null, Action<Block, int> click = null)
+        {
+            BlockMod = blockName;
+            Mod = _mod;
+            Position = new Vector2(_pos.X, _pos.Y);
+            Scale = new Vector2(_texture.Width, _texture.Height);
+            Texture = _texture;
+            _IndexTexture = index;
+            _GFX = _Parent;
+            OnRedraw += delegate(Block obj) { if(redraw != null) redraw(obj); };
+            OnDestroy += delegate(Block obj) { if (destroy != null) destroy(obj); };
+            OnClick += delegate (Block obj, int MouseClick) { if (click != null) click(obj, MouseClick); };
+            IsInteractive = click != null;
+        }
+        public void Execute(string code)
+        {
+            if (code != null && code !="")
+            {
+                CharpExecuter cs;
+                cs = new CharpExecuter(new ExecuteLogHandler(delegate { }));
+                cs.FormatSources(code);
+                cs.Execute();
+            }
+        }
+        public void draw() { _GFX.DrawImage(Texture, Position.x, Position.y); OnRedraw(this); }
+        public void Destroy() { OnDestroy.Invoke(this); }
+        public void Interact(MouseEventArgs e) {
+            int index = -1;
+            if (e.Button == MouseButtons.Left) index = 0;
+            if (e.Button == MouseButtons.Middle) index = 1;
+            if (e.Button == MouseButtons.Right) index = 2;
+            OnClick.Invoke(this, index);
+        }
+
+        public Dictionary<string, string> Properties = new Dictionary<string, string>();
+
+        public string GetProperty(string name)
+        {
+            if (Properties.ContainsKey(name))
+            {
+                return Properties[name];
+            }
+            return null;
+        }
+        public void SetProperty(string name, string value)
+        {
+            if (Properties.ContainsKey(name))
+            {
+                Properties[name] = value;
+            }
+            else
+            {
+                Properties.Add(name, value);
+            }
+        }
+    }
+    public class Inventar
+    {
+        public ModG MyMod;
+        public string ModName = "";
+        public string BlockName = "";
+        public Image _Texture;
+    }
+    public class ModG
+    {
+        public Action<Block> OnBlockPlaced = delegate(Block obj) { };
+        public Action<Block> OnDestroyed = delegate(Block obj) { };
+        public Action<ModG> OnBlockSelect = delegate(ModG obj) { };
+        public Action<Block> OnRedraw = delegate(Block obj) { };
+        public Action<Block, int> OnClick = null;
+
+        public ModManager.Mod Mod;
+        public string ModName = "";
+        public Image _Texture;
+        public string BlockName = "";
+    }
+}
---
+++ BuilderAPI_Scripts/ModManager.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Xml;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using LB_Launcher.Modules;
+using CharpShell;
+using System.Media;
+
+namespace LB_Launcher.Modules
+{
+    public static class ModManager
+    {
+        public static List<Mod> Mods = new List<Mod>();
+        public static Mod GetMod(string name)
+        {
+            return Mods.Where(mod => mod.Name == name).First();
+        }
+
+        public static Image[] _TextureLibrary =
+             { LB_Launcher.Properties.Resources._0,
+        LB_Launcher.Properties.Resources._1,
+        LB_Launcher.Properties.Resources._2,
+        LB_Launcher.Properties.Resources._3,
+        LB_Launcher.Properties.Resources._4_0,
+        LB_Launcher.Properties.Resources._5,
+        LB_Launcher.Properties.Resources._6,
+        LB_Launcher.Properties.Resources.brick_blue,
+        LB_Launcher.Properties.Resources.brick_floor,
+        LB_Launcher.Properties.Resources.brick_floor_blue,
+        LB_Launcher.Properties.Resources.brick_floor_brown,
+        LB_Launcher.Properties.Resources.brick_floor_russet,
+        LB_Launcher.Properties.Resources.sand_2,
+        LB_Launcher.Properties.Resources.Screenshot_145,
+        LB_Launcher.Properties.Resources.Screenshot_148,
+        LB_Launcher.Properties.Resources.zemla };
+
+        public static string CurrentMod, CurrentBlockName;
+
+        static string[,] methodListMod = {
+            { @"\script.cs", "LB_Launcher.Builder2D.OnModsLoaded += delegate" },
+            { @"\OnMapRedraw.cs", "LB_Launcher.Builder2D.Engine.OnMapRedraw += delegate" },
+            { @"\OnGlobalTimerTick.cs", "LB_Launcher.Builder2D.OnGlobalTimerUpdated += delegate" },
+            { @"\OnAbout.cs","LB_Launcher.Modules.ModManager.Modded.OnAbout = delegate(ModManager.Mod obj)" }
+        };
+        static string[,] methodBlocksMod = {
+            { @"\script.cs", "LB_Launcher.Modules.ModManager.TempMod.OnBlockPlaced = delegate(Block obj)" },
+            { @"\OnSelect.cs", "LB_Launcher.Modules.ModManager.TempMod.OnBlockSelect = delegate(ModG obj)" },
+            { @"\OnRedraw.cs", "LB_Launcher.Modules.ModManager.TempMod.OnRedraw = delegate(Block obj)" },
+            { @"\OnDestroy.cs", "LB_Launcher.Modules.ModManager.TempMod.OnDestroyed = delegate(Block obj)" },
+            { @"\OnClick.cs", "LB_Launcher.Modules.ModManager.TempMod.OnClick = delegate(Block obj, int MouseClick)" }
+        };
+
+        public static Mod Modded;
+        public static ModG TempMod;
+        public static async Task Init()
+        {
+            if (!Directory.Exists(Application.StartupPath + @"\2Dbuilder"))
+            { Directory.CreateDirectory(Application.StartupPath + @"\2Dbuilder"); }
+            if (!Directory.Exists(Application.StartupPath + @"\2Dbuilder\mods"))
+            { Directory.CreateDirectory(Application.StartupPath + @"\2Dbuilder\mods"); }
+
+            DirectoryInfo[] cDirs = new DirectoryInfo(Application.StartupPath + @"\2Dbuilder\mods").GetDirectories();
+            LBConsole.AddString("[Builder API] Загрузка информации о модах...");
+            Mods.Clear();
+            LBConsole.AddString("[Builder API] Список модов очищен!");
+            foreach (DirectoryInfo Mod in cDirs)
+            {
+                await Task.Run(() => {
+                    Mod ModedFile = new Mod(Mod.Name);
+                    Modded = ModedFile;
+
+                    for (int i = 0; i < methodListMod.GetUpperBound(0) + 1; i++)
+                    {
+                        if (File.Exists(Mod.FullName + methodListMod[i, 0]))
+                        {
+                            CharpExecuter cs;
+                            cs = new CharpExecuter(new ExecuteLogHandler(delegate { }));
+                            string code = File.ReadAllText(Mod.FullName + methodListMod[i, 0]);
+                            cs.FormatSources(methodListMod[i, 1] + " { " + code + " };");
+
+                            cs.Execute();
+                        }
+                    }
+                    
+                    Mods.Add(ModedFile);
+                    ModedFile.Path = Mod.FullName;
+                    ModedFile.ResourcePath = Mod.FullName + @"\Resources";
+
+                    DirectoryInfo[] Blocks = new DirectoryInfo(Application.StartupPath + @"\2Dbuilder\mods\" + Mod.Name).GetDirectories();
+                    foreach (DirectoryInfo block in Blocks)
+                    {
+                        if (block.Name == "Resources") continue;
+
+                        Image _texture = Image.FromFile(block.FullName + @"\texture.png");
+                        ModG newMod = new ModG();
+                        TempMod = newMod;
+                        newMod.ModName = Mod.Name; newMod._Texture = _texture;
+                        newMod.BlockName = block.Name;
+                        newMod.Mod = ModedFile;
+                        for (int i = 0; i < methodBlocksMod.GetUpperBound(0) + 1; i++)
+                        {
+                            if (File.Exists(block.FullName + methodBlocksMod[i, 0]))
+                            {
+                                CharpExecuter cs;
+                                cs = new CharpExecuter(new ExecuteLogHandler(delegate { }));
+                                string code = File.ReadAllText(block.FullName + methodBlocksMod[i, 0]);
+                                cs.FormatSources(methodBlocksMod[i, 1] + " { " + code + " };");
+                                cs.Execute();
+                            }
+                        }
+                        ModedFile.Blocks.Add(newMod);
+                    }
+
+                });
+                LBConsole.AddString($"[Builder API] {Mod.Name} Инициализирован.");
+            }
+        }
+
+        public class Mod
+        {
+            public Action<Mod> OnAbout = null;
+
+            public string Name;
+            public string Path;
+            public string ResourcePath;
+
+            public List<ModG> Blocks = new List<ModG>();
+            public ModG GetBlock(string name)
+            {
+                return Blocks.Where(mod => mod.BlockName == name).First();
+            }
+            public Mod(string _modName, List<ModG> blocks)
+            {
+                Name = _modName;
+                Blocks = blocks;
+            }
+            public Mod(string _modName)
+            {
+                Name = _modName;
+            }
+
+            Dictionary<string,object> Properties = new Dictionary<string, object>();
+
+            public object GetProperty(string name)
+            {
+                if (Properties.ContainsKey(name))
+                {
+                    return Properties[name];
+                }
+                return null;
+            }
+            public void SetProperty(string name, object value)
+            {
+                if (Properties.ContainsKey(name))
+                {
+                    Properties[name] = value;
+                }
+                else
+                {
+                    Properties.Add(name, value);
+                }
+            }
+
+            public string GetTextFromResources(string FileName)
+            {
+                if (File.Exists(ResourcePath + @"\" + FileName))
+                {
+                    return File.ReadAllText(ResourcePath + @"\" + FileName);
+                }
+                return null;
+            }
+            public Image GetImageFromResources(string FileName)
+            {
+                if (File.Exists(ResourcePath + @"\" + FileName))
+                {
+                    return Image.FromFile(ResourcePath + @"\" + FileName);
+                }
+                return null;
+            }
+            public byte[] GetBytesFromResources(string FileName)
+            {
+                if (File.Exists(ResourcePath + @"\" + FileName))
+                {
+                    return File.ReadAllBytes(ResourcePath + @"\" + FileName);
+                }
+                return null;
+            }
+            public SoundPlayer GetSoundFromResources(string FileName)
+            {
+                if (File.Exists(ResourcePath + @"\" + FileName))
+                {
+                    return new SoundPlayer(ResourcePath + @"\" + FileName);
+                }
+                return null;
+            }
+            public void SetTextToResources(string FileName, string content)
+            {
+                File.WriteAllText(ResourcePath + @"\" + FileName, content);
+            }
+            public void SetImageToResources(string FileName, Image content)
+            {
+                content.Save(ResourcePath + @"\" + FileName);
+            }
+            public void SetBytesToResources(string FileName, byte[] content)
+            {
+                File.WriteAllBytes(ResourcePath + @"\" + FileName, content);
+            }
+        }
+    }
+}
